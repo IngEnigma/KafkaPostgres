6fe8a0dd24e18da6c733f670909a590b8e4b113e
IngEnigma <159200281+IngEnigma@users.noreply.github.com>
2025-03-27 23:08:46 -0600

    Update kafkaConsumer.py

--- kafkaConsumer.py
+++ kafkaConsumer.py
@@ -12,3 +12,19 @@ try:
     print("PosgreSql Connected successfully!")
 except:
     print("Could not connect to PosgreSql")
+
+consumer = KafkaConsumer('people',bootstrap_servers=['localhost:9092'])
+
+for msg in consumer:
+    record = json.loads(msg.value).replace('"','').split(',')
+    name = record[0].split(':')[1]
+    birth = record[1].split(':')[1]
+    try:
+       sql = "INSERT INTO people(name, birth) VALUES('" + name + "', '" +  birth + "')"
+       print(sql)
+       cur.execute(sql)
+       conn.commit()
+  except:
+       print("Could not insert into PostgreSql")
+conn.close()
+
