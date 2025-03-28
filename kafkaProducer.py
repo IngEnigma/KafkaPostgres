0469623630bae1995f4ddb9d1bc6a7e0b1104914
IngEnigma <159200281+IngEnigma@users.noreply.github.com>
2025-03-27 23:16:09 -0600

    Update kafkaProducer.py

--- kafkaProducer.py
+++ kafkaProducer.py
@@ -1,28 +1,25 @@
 from kafka import KafkaProducer
+import pandas as pd
 import json
 
 producer = KafkaProducer(
    bootstrap_servers='localhost:9092',
-   value_serializer=lambda v: json.dumps(v).encode('utf-8')
+   value_serializer=lambda v: json.dumps(v).encode('utf-8')  
 )
 
 def on_send_success(record_metadata):
-   print(record_metadata.topic)
-   print(record_metadata.partition)
-   print(record_metadata.offset)
+   print(f'Topic: {record_metadata.topic}, Partition: {record_metadata.partition}, Offset: {record_metadata.offset}')
 
 def on_send_error(excp):
-   print('I am an errback', exc_info=excp)
+   print('Error al enviar mensaje:', excp)
 
+url = 'https://raw.githubusercontent.com/IngEnigma/StreamlitSpark/refs/heads/master/results/male_crimes/part-00000-8b3d0568-ab40-4980-a6e8-e7e006621725-c000.json'
 
-url='https://raw.githubusercontent.com/adsoftsito/bigdata/refs/heads/main/results/data.json'
-import pandas as pd
+df = pd.read_json(url, lines=True)
 
-df = pd.read_json(url, orient='columns')
-
-for index, value in df.head(3).iterrows():
-   dict_data = dict(value)
-   producer.send('people', value=dict_data[0])
-   print(dict_data[0])
+for _, row in df.iterrows():  
+   data = row.to_dict()  
+   producer.send('crime', value=data).add_callback(on_send_success).add_errback(on_send_error)
+   print(f'Enviado: {data}') 
 
 producer.close()
