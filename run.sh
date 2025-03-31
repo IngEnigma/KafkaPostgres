868d42df8c8de60a554e554dcf8fb9e26ec27906
IngEnigma <159200281+IngEnigma@users.noreply.github.com>
2025-03-31 00:09:59 -0600

    Update and rename start.sh to run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/opt/kafka/bin/zookeeper-server-start.sh /opt/kafka/config/zookeeper.properties &
+
+sleep 10
+
+/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties &
+
+sleep 15
+
+/opt/kafka/bin/kafka-topics.sh --create --if-not-exists \
+    --bootstrap-server localhost:9092 \
+    --replication-factor 1 \
+    --partitions 3 \
+    --topic crime_records
+
+python3 /app/producer/kafkaProducer.py &
+python3 /app/consumer/kafkaConsumer.py &
+
+tail -f /dev/null
