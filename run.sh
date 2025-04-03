a64d25c02af7c9f6b55cc2d89ce8aabc1a444745
IngEnigma <159200281+IngEnigma@users.noreply.github.com>
2025-04-03 10:29:26 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Iniciar Zookeeper en segundo plano
 kafka_2.12-3.7.2/bin/zookeeper-server-start.sh kafka_2.12-3.7.2/config/zookeeper.properties &
 
-# Esperar a que Zookeeper esté listo
 sleep 5
 
-# Iniciar Kafka en segundo plano
 kafka_2.12-3.7.2/bin/kafka-server-start.sh kafka_2.12-3.7.2/config/server.properties &
 
-# Esperar a que Kafka esté listo
 sleep 10
 
-# Crear el topic si no existe
 kafka_2.12-3.7.2/bin/kafka-topics.sh --create \
     --bootstrap-server localhost:9092 \
     --replication-factor 1 \
@@ -20,14 +15,10 @@ kafka_2.12-3.7.2/bin/kafka-topics.sh --create \
     --topic crime || \
     echo "El topic crime_records ya existe o no se pudo crear"
 
-# Iniciar el consumidor en segundo plano
 python3 consumer.py &
 
-# Esperar un momento antes de iniciar el productor
 sleep 5
 
-# Iniciar el productor
 python3 producer.py
 
-# Mantener el contenedor en ejecución
 tail -f /dev/null
